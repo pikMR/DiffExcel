016404ae0694cd136c2b71bd821faf6add7da507
pikmr <benjahergar@gmail.com>
2021-05-25 20:33:48 +0200

    Creación de excel, SqlConnection + SqlDataAdapter

---
+++ DocumentTasks.cs
@@ -0,0 +1,26 @@
+﻿using ClosedXML.Excel;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.SqlClient;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DiffExcel
+{
+    public class DocumentTasks
+    {
+        public static void CreateExcelWithTableName(string table)
+        {
+            var wb = new XLWorkbook();
+            using SqlConnection connection = new(DbConnectionTarget.ConnectionStringTrusted);
+            using var da = new SqlDataAdapter($"select * from {table}", connection);
+            DataTable datatable = new();
+            da.Fill(datatable);
+            datatable.TableName = table;
+            wb.Worksheets.Add(datatable);
+            wb.SaveAs($"{table}.xlsx");
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,6 +1,9 @@
-﻿using Microsoft.Extensions.Configuration;
+﻿using ClosedXML.Excel;
+using Microsoft.Extensions.Configuration;
 using System;
+using System.Data;
 using System.Data.SqlClient;
+using System.Linq;
 
 namespace DiffExcel
 {
@@ -9,8 +12,7 @@ namespace DiffExcel
         static void Main(string[] args)
         {
             ConfigurationDB();
-            GetConnectionDBOK();
-            GetDataDBOK();
+            DocumentTasks.CreateExcelWithTableName(DbConnectionTarget.Tables[0]);
         }
 
         public static void ConfigurationDB()
@@ -19,47 +21,5 @@ namespace DiffExcel
                 .AddJsonFile(Utils.getRootPath("appsettings.json"))
                 .Build();
         }
-
-        public static void GetConnectionDBOK()
-        {
-            using (SqlConnection connection = new SqlConnection(
-               DbConnectionTarget.ConnectionStringTrusted))
-            {
-                SqlCommand command = new SqlCommand("select * from Person.BusinessEntity", connection);
-                command.Connection.Open();
-                command.ExecuteNonQuery();
-            }
-        }
-
-        public static void GetDataDBOK()
-        {
-            SqlDataReader rdr = null;
-
-            try
-            {
-                using (SqlConnection connection = new SqlConnection(
-               DbConnectionTarget.ConnectionStringTrusted))
-                {
-                    SqlCommand command = new SqlCommand("select * from Person.BusinessEntity", connection);
-                    command.Connection.Open();
-                    rdr = command.ExecuteReader();
-                    // print the CustomerID of each record
-                    while (rdr.Read())
-                    {
-                        Console.WriteLine(rdr[0]);
-                    }
-                    //command.ExecuteNonQuery();
-                }
-            }
-            finally
-            {
-                // close the reader
-                if (rdr != null)
-                {
-                    rdr.Close();
-                }
-            }
-        }
-
     }
 }
